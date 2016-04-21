7c79dac744ba2fba24dbad2b5844302a9bc4d69d
Matthew <m45yang@uwaterloo.ca>
2016-04-20 20:27:09 -0400

    changed get to post

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ function sendTextMessage(sender, text) {
     })
 }
 
-app.get('/webhook/', function (req, res) {
+app.post('/webhook/', function (req, res) {
     messaging_events = req.body.entry[0].messaging
     for (i = 0; i < messaging_events.length; i++) {
         event = req.body.entry[0].messaging[i]
