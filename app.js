0250e5dcec6641e9c64148c98358d37dd075348a
Matthew <m45yang@uwaterloo.ca>
2016-07-01 16:11:37 -0400

    ultimate error handler

--- app.js
+++ app.js
@@ -49,6 +49,10 @@ app.post('/webhook/', function (req, res) {
     .then(function(context) {
       return Chevy.reply(sender, context.replies)
     })
+    .catch(function(err) {
+      console.log(err)
+      return Chevy.reply(sender, ['Something went wrong, please try again!'])
+    })
   }
 
   res.sendStatus(200)
@@ -57,7 +61,7 @@ app.post('/webhook/', function (req, res) {
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
   if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-      res.send(req.query['hub.challenge'])
+      return res.send(req.query['hub.challenge'])
   }
   res.send('Error, wrong token')
 })
